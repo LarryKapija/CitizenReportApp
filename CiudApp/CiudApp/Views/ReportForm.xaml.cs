c363109bc23471f680ef5d77cd29d9f4415e2d62
LarryKapija <amcaballero2023@gmail.com>
2021-01-07 13:15:49 -0400

    All UX Changes DONE

--- CiudApp/CiudApp/Views/ReportForm.xaml.cs
+++ CiudApp/CiudApp/Views/ReportForm.xaml.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace CiudApp.Views.Pages
+namespace CiudApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class CalendarPage : ContentPage
+    public partial class ReportForm : ContentPage
     {
-        public CalendarPage()
+        public ReportForm()
         {
             InitializeComponent();
         }
