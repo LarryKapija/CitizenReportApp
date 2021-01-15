80d67293dfcd9f5eb313cf28fc02333db2cbd8b3
Derick <derick290801@gmail.com>
2021-01-14 23:09:51 -0400

    Creating styles, entry view added

---
+++ CiudApp/CiudApp/Views/Views/DataEntry.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CiudApp.Views.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DataEntry : Grid
+    {
+        public DataEntry()
+        {
+            InitializeComponent();
+        }
+    }
+}
